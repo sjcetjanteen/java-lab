32d5418b7043665c107e518846d55bab2e311b5e
sjcetjanteen <101170582+sjcetjanteen@users.noreply.github.com>
2022-04-01 14:05:56 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reverse;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sjcet
+ */
+public class Reverse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char a;
+        Scanner d.new Scanner(System.in);
+        System.out.println("enter a string");
+        a=d.next
+        
+        // TODO code application logic here
+    }
+    
+}
